9d1a329aded92be42aa5f9b69e1ea25b544169c3
Paulo Salvatore <salvatoregames@gmail.com>
2022-03-03 12:11:43 -0300

    Formatação do código com prettier

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
 
-const herois = ['Mulher Maravilha', 'Capitã Marvel', 'Homem de Ferro']
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
 
-app.get('/herois', function (req, res) {
-    res.send(herois)
-})
+app.get("/herois", function (req, res) {
+    res.send(herois);
+});
 
-app.listen(3000)
+app.listen(3000);
