cb89a970b50cf4c0423ab5d88e8ff0c280d57665
Paulo Salvatore <salvatoregames@gmail.com>
2022-03-04 10:34:53 -0300

    Endpoint Read by ID implementado

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ app.get("/", function (req, res) {
 });
 
 const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//              0                    1                2
 
+// [GET] Read All (Ler individualmente)
 app.get("/herois", function (req, res) {
     res.send(herois);
+});
+
+// [GET] Read Single (by Id) (Ler individualmente pelo ID)
+app.get("/herois/:id", function (req, res) {
+    // Acesso o parâmetro da rota chamado ID
+    const id = req.params.id - 1;
+
+    // Pega o item da lista que corresponde a esse ID
+    const item = herois[id];
+
+    res.send(item);
 });
 
 app.listen(3000);
